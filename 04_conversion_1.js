17970440294432544ab6db9954106e37637f7947
Simran Kaushal <simrankaushal374@gmail.com>
2025-07-08 14:36:34 +0530

    conversion

---
+++ 04_conversion_1.js
@@ -0,0 +1,14 @@
+//why string to number conversion is confusing
+
+let value = 33
+
+let negValue= -value
+console.log(negValue);
+
+let str1 ="hello"
+
+let str2= " simran"
+let str3 = str1+ str2
+
+console.log(str3);
+
