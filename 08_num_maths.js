e5760a243898ae6b9cf04909f2485933d3f13738
Simran Kaushal <simrankaushal374@gmail.com>
2025-07-11 21:10:19 +0530

    maths

--- 08_num_maths.js
+++ 08_num_maths.js
@@ -1,1 +1,45 @@
-console.log("Hello, World!");
+// const num =10
+// console.log(num);
+
+// // const num1=20
+// const num2 = new Number(20);
+// console.log(num2); //with number keyword
+// //console.log(num1);//without number keyword
+
+
+// console.log( num.toString().length); //convert number to string and get length
+// console.log( num2.toFixed(3)); //convert number to string with 3 decimal places
+
+//  //convert number to string
+
+
+//  const otherNum = 214.8923;
+// console.log(otherNum.toPrecision(3)); //convert number to string with 5 significant digits
+
+
+// const num3 = 1000000
+// console.log(num3.toLocaleString('en-IN')); //convert number to string with locale formatting
+
+//************************************************************************************************************ */
+
+// MATHS
+
+console.log(Math.PI); // PI value
+
+console.log(Math.abs(-4)); // absolute value
+console.log(Math.ceil(4.2)); // round up
+console.log(Math.floor(4.8)); // round down
+console.log(Math.round(3.5)); // round to nearest integer   
+
+console.log(Math.max(1, 2, 3, 4, 5)); // maximum value
+console.log(Math.min(1, 2, 3, 4, 5)); // minimum value
+console.log(Math.random()); // random number between 0 and 1
+console.log(Math.random() * 10); // random number between 0 and 10
+console.log(Math.random() * 10 + 1); // random number between 1 and 10
+console.log(Math.floor(Math.random() * 10 + 1)); // random integer between 1 and 10
+console.log(Math.sqrt(16)); // square root
+console.log(Math.pow(2, 3)); // power of a number (2^3 = 8)
+
+
+
+
