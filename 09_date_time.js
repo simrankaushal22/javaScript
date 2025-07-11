269410c14f2f84ce84f62813a57999af32062de0
Simran Kaushal <simrankaushal374@gmail.com>
2025-07-11 21:43:46 +0530

    date

---
+++ 09_date_time.js
@@ -0,0 +1,19 @@
+// let myDate = new Date();
+// console.log(myDate.toString()); // current date and time in string format
+// console.log(myDate.toDateString()); // current date in string format
+// console.log(myDate.toISOString()); // current date and time in ISO format
+// console.log(myDate.toJSON()); // current date and time in JSON format
+// console.log(myDate.toLocaleString()); // current date and time in locale string format
+// console.log(myDate.toLocaleDateString()); // current date in locale date string format
+// console.log(typeof myDate); // type of myDate variable
+
+// let myCreatedDate = new Date('2023-10-23'); // create a date from a string
+// let myCreatedDate2 = new Date(2024,0,23,5,3);
+//let myCreatedDate3 = new Date("2024=11-14");
+// let myCreatedDate3 = new Date("07-11-2025");
+// console.log(myCreatedDate3.toLocaleString()); // date from string in string format
+ 
+
+let myTimestamp = Date.now(); // get current timestamp in milliseconds
+console.log(myTimestamp); // current timestamp in milliseconds
+
