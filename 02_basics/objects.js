af148d9a6e28d82547514fb99f50bc5e381cc88f
Simran Kaushal <simrankaushal374@gmail.com>
2025-07-13 22:47:04 +0530

    objects

--- 02_basics/objects.js
+++ 02_basics/objects.js
@@ -14,6 +14,8 @@ console.log( ram.class);
 console.log(ram['rollNo']);
 console.log(ram["location"]);
 
+
+
 console.log(ram[mySym]); //accessing value using symbol key
 
 //freezing object
@@ -28,3 +30,42 @@ ram.grettingTwo= function(){
     console.log(`hello ${this.name}`);
 }
 console.log(ram.grettingTwo()); 
+
+
+const  person={
+    name:"simran",
+    age:23,
+    city:"shimla",
+    "class":"mca",
+}
+console.log(person.name);
+console.log(person.class);
+console.log(person["class"]);
+
+person["age"]=25
+console.log(person.age); //updating value of age
+
+
+person.color =" blue"
+console.log(person.color); //adding new key-value pair
+
+delete person.class
+console.log(person.class); //deleting key-value pair
+
+const calculator ={
+add:("a,b"),
+sub:("a,b"),
+mul:("a,b"),
+div:("a,b"),
+
+ } 
+
+ console.log(calculator.add);
+ 
+
+ let book={
+title:"JavaScript Basics",
+author:"John Doe", 
+}
+console.log("title" in book); //checking if key exists using 'in' operator
+console.log("publisher" in book); //checking if key exists using 'in' operator
